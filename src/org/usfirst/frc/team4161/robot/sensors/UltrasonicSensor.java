8bb4b99b04df75f6d0e05926800b2555587c2141
Micah Raney <MicahRaney@users.noreply.github.com>
2017-02-10 17:53:49 -0800

    Added class for MaxSonar UltrasonicSensor
    The UltrasonicSensor class is built based on the analog voltage model,
    but modeled specifically for the MaxSonar MB1013 Ultrasonic Sensor
    model.

---
+++ src/org/usfirst/frc/team4161/robot/sensors/UltrasonicSensor.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team4161.robot.sensors;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+/**
+ * Ultrasonic sensor wrapper library for the MB1013 Ultrasonic Sensor. 
+ * @author Micah Raney
+ *
+ */
+public class UltrasonicSensor {
+
+	private AnalogInput ultrasonic;
+	/**
+	 * The number of bits of oversampling, as described in the AnalogInput api.
+	 * Average sets the average number of bits.
+	 */
+	private static int oversample = 16, average = 4;
+	
+	/**
+	 * millimeters per volt.
+	 */
+	private double scalingFactor = 5120/5;
+
+	/**
+	 * Initializes the Ultrasonic sensor based on the given analog port.
+	 * @param analogPort Analog port that the sensor is based on.
+	 */
+	public UltrasonicSensor(AnalogInput analogPort) {
+		ultrasonic = analogPort;
+		ultrasonic.setOversampleBits(oversample);
+		ultrasonic.setAverageBits(average);
+	}
+	
+	
+	/**
+	 * Returns the sensed distance, in meters.
+	 * @return Meter distance sensed by the sensor
+	 */
+	public double getMDistance(){
+		double distance = ultrasonic.getAverageVoltage()/oversample;
+		distance *= scalingFactor;//scale to millimeters
+		distance /= 1000;//scale to meters
+		return distance;
+	}
+	
+	
+
+}
