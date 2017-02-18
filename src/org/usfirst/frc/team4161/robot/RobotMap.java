90d616e6b3ecc2a0acbdb61e956ab3db3a1b7912
Micah Raney <MicahRaney@users.noreply.github.com>
2017-02-18 13:33:00 -0800

    Set motor controller ids

--- src/org/usfirst/frc/team4161/robot/RobotMap.java
+++ src/org/usfirst/frc/team4161/robot/RobotMap.java
@@ -23,8 +23,8 @@ public class RobotMap {
 	// number and the module. For example you with a rangefinder:
 	// public static int rangefinderPort = 1;
 	// public static int rangefinderModule = 1;
-	private final static int LWheelsID = 0, RWheelsID = 1, HopperActuatorID = 2, ShooterMotorID = -1,
-			SweeperMotorID = -1;
+	private final static int LWheelsID = 0, RWheelsID = 1, HopperActuatorID = 2, ShooterMotorID = 3,
+			SweeperMotorID = 4;
 	
 	public static Spark LWheels = new Spark(LWheelsID), RWheels = new Spark(RWheelsID);
 	
